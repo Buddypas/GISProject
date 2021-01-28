611ed232fcf96a775dc6296eb63c8c45bb484eb7
Nikola Ivanovic <nikola.ivanovic@amplitudo.me>
2021-01-28 22:58:32 +0100

    Edited docs folder

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox:{
+    accessToken:'pk.eyJ1IjoiYnVkZHlwYXMiLCJhIjoiY2tqZ2QxYno3M3ZqOTJ5bnFvdXV0Y3UyeSJ9.b3pGFuaPFNU3l2mkgGmSBw'
+  }
 };
